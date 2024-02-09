670fea999de38085afefab9001bf252386e7edaf
zjc2106 <zjc2106@columbia.edu>
2024-02-09 11:38:10 -0800

    sorta working

--- lab1/lab1.sv
+++ lab1/lab1.sv
@@ -18,14 +18,19 @@ module lab1( input logic        CLOCK_50,  // 50 MHz Clock input
 	     );
 
     logic 			clk, go, done;   
-    logic [31:0] 		start;
+    logic [31:0] 		start=1;
     logic [15:0] 		count;
  
     logic [23:0]                go_counter=1;
+    logic [23:0]                up_counter=1;
+    logic [23:0]                down_counter=1;
+    logic [23:0]                reset_counter=1;
     logic [3:0]                 press_counter=0;
-    logic                       waiting=0;
+    logic [1:0]                 waiting=0;
     logic                       done_trig = 0;
     logic [15:0]                count_output=0;
+    logic [15:0]		display_output= 0;
+    logic [15:0]	 	n=0;
 
     assign clk = CLOCK_50;
    
@@ -34,24 +39,26 @@ module lab1( input logic        CLOCK_50,  // 50 MHz Clock input
  
 
     // display switch value on 7 seg display
-    hex7seg h1(.a(SW[3:0]), .y(HEX3[6:0]));       
-    hex7seg h2(.a(SW[7:4]), .y(HEX4[6:0]));       
-    //hex7seg h3(.a({2'b00, SW[9:8]}), .y(HEX5[6:0]));
+   // hex7seg h1(.a(SW[3:0]), .y(HEX3[6:0]));       
+   // hex7seg h2(.a(SW[7:4]), .y(HEX4[6:0]));       
+   // hex7seg h3(.a({2'b00, SW[9:8]}), .y(HEX5[6:0]));
 
-
+   hex7seg h1(.a(display_output[3:0]), .y(HEX3[6:0]));
+   hex7seg h2(.a(display_output[7:4]), .y(HEX4[6:0]));
+   hex7seg h3(.a(display_output[11:8]), .y(HEX5[6:0]));
 
     hex7seg h4(.a(count_output[3:0]), .y(HEX0[6:0]));
 
     hex7seg h5(.a(count_output[7:4]), .y(HEX1[6:0]));
 
-    hex7seg h6(.a(count_output[11:8], .y(HEX2[6:0]));
+    hex7seg h6(.a(count_output[11:8]), .y(HEX2[6:0]));
 
     always_ff @(posedge clk) begin
         go<=0;
-        waiting<=0;
-        if (waiting == 1)
+	if (waiting == 1 || waiting == 2) waiting <= waiting + 2'b1;
+        if (waiting == 3)
         begin
-            count_output <= (count-1);
+            count_output <= (count);
             waiting <= 0;
         end
         if (~KEY[3])
@@ -60,22 +67,60 @@ module lab1( input logic        CLOCK_50,  // 50 MHz Clock input
             begin 
                 go <= 1;
                 start <= {22'b0, SW[9:0]};
+		display_output <= {5'b0, SW[9:0]};
+		n <= {5'b0, SW[9:0]};
                 go_counter <= 24'b1;
                 press_counter <= press_counter + 1;
                 done_trig <= 0;
             end 
             else begin
-                go_counter <= go_counter +  24'b1;
-                go <= 0;
+                go_counter <= go_counter + 24'b1;
             end
         end
         else go_counter <= 0;
-        if (done == 1) 
+        if (done == 1 && ~done_trig) 
         begin
             start <= 0;
             waiting <= 1;
             done_trig <= 1;
         end
+	if(~KEY[0])
+	begin
+		if(up_counter == 24'b0)
+		begin
+			start <= start + 32'b1;
+			up_counter <=  24'b1; 
+			waiting <= 1;
+			display_output <= display_output + 15'b1;	
+		end
+		else up_counter <= up_counter + 24'b1;
+	end
+	else up_counter <= 0;
+	if(~KEY[1])
+	begin
+		if(down_counter == 24'b0)
+		begin
+			start <= start - 32'b1;
+			down_counter <=  24'd5000000000; 
+			waiting <= 1;
+			display_output <= display_output - 15'b1;	
+		end
+		else down_counter <= down_counter + 24'b1;
+	end
+	else down_counter <= 0;
+
+	if(~KEY[2])
+	begin
+		if(reset_counter == 24'b0)
+		begin
+			start <= 32'b0;
+			reset_counter <=  24'b1; 
+			waiting <= 1;
+			display_output <= n;	
+		end
+		else reset_counter <= reset_counter + 24'b1;
+	end
+	else reset_counter <= 0;
     end
 
  endmodule
