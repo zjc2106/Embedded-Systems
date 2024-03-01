22b31f46b00f1452755cdd21a0f9be61bda51947
= <sanjay.rajasekharan@gmail.com>
2024-03-01 16:27:37 -0500

    tweak

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -222,7 +222,7 @@ int main()
 
         else if (packet.keycode[0] == LEFT_ARROW) {
           if (cursor > 0) {
-            if (user_col == FIRST_COL) {
+            if (user_col <= FIRST_COL) {
               user_col = last_col;
               user_row--;
             } else user_col--;
